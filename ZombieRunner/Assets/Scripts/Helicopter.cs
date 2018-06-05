a8e0d59289aabe1064406c96aba0410a69cc67e2
Jamie Page <animate@formtactics.com>
2018-06-05 13:01:01 +0700

    Finishing Tutorial

---
+++ ZombieRunner/Assets/Scripts/Helicopter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+    
+
+    private bool called = false;
+    private Rigidbody Rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+        
+    }
+
+    void OnDisatchHelicopter ()
+    {
+            
+        Debug.Log("Helicopter called");
+        Rigidbody.velocity = new Vector3(0, 0, 50f);
+        called = true;
+    
+    }
+}
